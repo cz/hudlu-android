c054e7317777cca0c597ad6ee673e3eaf464b1d3
Craig Zheng <craig.zheng@gmail.com>
2015-11-23 20:10:13 -0500

    Add serializedName annotation

--- app/src/main/java/zheng/craig/hudlu/models/MashableNews.java
+++ app/src/main/java/zheng/craig/hudlu/models/MashableNews.java
@@ -1,10 +1,13 @@
 package zheng.craig.hudlu.models;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 /**
  * Created by cz on 11/23/15.
  */
 public class MashableNews {
-    public List<MashableNewsItem> newsItems;
+    // Set serialized JSON field name
+    @SerializedName("new") public List<MashableNewsItem> newsItems;
 }
