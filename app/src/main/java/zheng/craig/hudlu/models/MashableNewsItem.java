10e1faaec7bba78c0a5ff2cda1c09cf6e4b0069c
Craig Zheng <craig.zheng@gmail.com>
2015-11-23 20:02:02 -0500

    Add MashableNewsItem model

---
+++ app/src/main/java/zheng/craig/hudlu/models/MashableNewsItem.java
@@ -0,0 +1,11 @@
+package zheng.craig.hudlu.models;
+
+/**
+ * Created by cz on 11/23/15.
+ */
+public class MashableNewsItem {
+    public String title;
+    public String author;
+    public String link;
+    public String image;
+}
