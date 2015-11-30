f4071856d4b0e7c8e51acef88cc9acd51632a1e9
Craig Zheng <craig.zheng@gmail.com>
2015-11-29 22:46:37 -0500

    Start building out Favorites Utility

--- app/src/main/java/zheng/craig/hudlu/FavoriteUtil.java
+++ app/src/main/java/zheng/craig/hudlu/FavoriteUtil.java
@@ -1,7 +1,39 @@
 package zheng.craig.hudlu;
 
+import android.content.Context;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+import zheng.craig.hudlu.models.Favorite;
+import zheng.craig.hudlu.models.MashableNewsItem;
+
 /**
  * Created by cz on 11/29/15.
  */
 public class FavoriteUtil {
+    public static void addFavorite(Context context, MashableNewsItem newsItem) {
+        Favorite favorite = new Favorite();
+        favorite.title = newsItem.title;
+        favorite.image = newsItem.image;
+        favorite.author = newsItem.author;
+        favorite.link = newsItem.link;
+
+        Realm realm = Realm.getInstance(context);
+
+        realm.beginTransaction();
+        realm.copyToRealm(favorite);
+        realm.commitTransaction();
+    }
+
+    public static void removeFavorite(Context context, MashableNewsItem newsItem) {
+
+    }
+
+    public static boolean isFavorite(Context context, MashableNewsItem newsItem) {
+
+    }
+
+    public static RealmResults<Favorite> getAllFavorites(Context context) {
+
+    }
 }
--- app/src/main/java/zheng/craig/hudlu/models/Favorite.java
+++ app/src/main/java/zheng/craig/hudlu/models/Favorite.java
@@ -6,8 +6,8 @@ import io.realm.RealmObject;
  * Created by cz on 11/29/15.
  */
 public class Favorite extends RealmObject {
-    private String title;
-    private String author;
-    private String image;
-    private String link;
+    public String title;
+    public String author;
+    public String image;
+    public String link;
 }
