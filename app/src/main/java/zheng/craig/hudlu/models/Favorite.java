b4f9aa34410ce9d70725a49d2b2cc63cfb5eae7f
Craig Zheng <craig.zheng@gmail.com>
2015-11-29 22:23:42 -0500

    Add Favorite class

---
+++ app/src/main/java/zheng/craig/hudlu/models/Favorite.java
@@ -0,0 +1,13 @@
+package zheng.craig.hudlu.models;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by cz on 11/29/15.
+ */
+public class Favorite extends RealmObject {
+    private String title;
+    private String author;
+    private String image;
+    private String link;
+}
